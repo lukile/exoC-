6903bd4443132f12d5471f0971eacf59bb954eaf
Lukile <lucile.1988.ls@gmail.com>
2019-08-30 17:17:39 +0200

    Added files

---
+++ cmake-build-debug/CMakeFiles/exoC__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exoC__.dir/main.cpp.o"
+  "exoC__.pdb"
+  "exoC__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exoC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T product(T t, int N) {
+    ‌​‌﻿‌​​‍‌
+        t;
+}
+
+
+int main() {
+    int N = 5;
+    int t = 4;
+    int k;
+
+    k = product<int>(t, N);
+
+    std::cout << k << endl;
+}
